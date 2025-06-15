5a58604f218f45bf75754e34b1fb0bcb5fc94863
Jainish Jain <jainishjain.1105@gmail.com>
2025-06-15 08:24:15 +0530

    Projects updated

--- src/app/projects/data.ts
+++ src/app/projects/data.ts
@@ -1,4 +1,12 @@
-import { Bot, Stethoscope, Plane, Music } from "lucide-react";
+import {
+  Bot,
+  Stethoscope,
+  Plane,
+  Music,
+  Heart,
+  Syringe,
+  Pill,
+} from "lucide-react";
 import { Project } from "../../types/project";
 
 export const projects: Project[] = [
@@ -30,6 +38,36 @@ export const projects: Project[] = [
     tags: ["Java", "Kotlin", "XML", "Firebase"],
     icon: Plane,
     color: "bg-sky-600",
-    githubRepo: "https://github.com/jainishjain11", // Replace with actual repo link if available
+    githubRepo: "https://github.com/jainishjain11", // Replace with actual repo link
+  },
+  {
+    id: 4,
+    name: "HealthAdmin",
+    description:
+      "Admin portal for managing healthcare systems including doctors, patients, and appointments. Focused on secure CRUD operations and analytics.",
+    tags: ["React", "Node.js", "MongoDB", "JWT"],
+    icon: Heart,
+    color: "bg-pink-600",
+    githubRepo: "https://github.com/jainishjain11/HealthAdmin",
+  },
+  {
+    id: 5,
+    name: "PillPal",
+    description:
+      "A smart pill reminder app that notifies patients of medication schedules. Includes dose tracking and adherence monitoring.",
+    tags: ["Android", "Java", "SQLite"],
+    icon: Pill,
+    color: "bg-purple-500",
+    githubRepo: "https://github.com/jainishjain11/PillPal",
+  },
+  {
+    id: 6,
+    name: "Music-System",
+    description:
+      "A web-based music system using audio APIs and playlists to explore, search, and play songs. Part of a front-end mini-project.",
+    tags: ["Python", "MySQL",],
+    icon: Music,
+    color: "bg-yellow-500",
+    githubRepo: "https://github.com/jainishjain11/Music-System",
   },
 ];
